ec00f890648a7283a99c062595753fcd7599654a
myzerly <nic.edwrds@gmail.com>
2016-02-09 16:48:50 -0800

    fixes js error that was causing links to populate incorrectly for case studies

--- js/app.js
+++ js/app.js
@@ -84,7 +84,7 @@ var cases = [
     imageTile: "tile-health",
     buttonCopy: "Keynote Slides",
     tileTitle: "Design Challenge: Apple Health",
-    caseLink: "http://myzerly.com/img/apple-health.pdf"
+    caseLink: "img/apple-health.pdf"
   }
 ];
 
